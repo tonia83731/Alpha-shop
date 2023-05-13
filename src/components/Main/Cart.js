cdda5e0848a9990ee805246442bf35fcdf308c1f
tonia94731 <huiyu.tonia.chang@gmail.com>
2023-05-13 18:35:58 +0800

    Add plus/minus event handler for the Cart

--- src/components/Main/Cart.js
+++ src/components/Main/Cart.js
@@ -3,83 +3,103 @@ import JeansTwo from '../Assets/images/product-2.jpg'
 import { ReactComponent as IconPlus } from "../Assets/icons/plus.svg";
 import { ReactComponent as IconMinus } from "../Assets/icons/minus.svg";
 
-const cartItem = [
+import {useState} from 'react';
+
+const initialCartItems = [
   {
     id:1,
     name: "破壞補丁修身牛仔褲",
-    price: "$3,999",
+    price: 3999,
     img:JeansOne,
     quantity: 1
   },
   {
     id: 2,
     name: "刷色直筒牛仔褲",
-    price: "$1,299",
+    price: 1299,
     img: JeansTwo,
     quantity: 1
   }
 ]
 
-const cartInfo = [
-  {
-    id: 1,
-    type: 'cart-info shipping col col-12',
-    text: '運費',
-    price: '免費'
-  },
-  {
-    id: 2,
-    type: 'cart-info total col col-12',
-    text: '小計',
-    price: '$5,298'
+function CartInfo({type, text, price}){
+  return(
+    <section className={`cart-info ${type} col col-12`} key={type}>
+      <div className="text">{text}</div>
+      <div className="price">{price}</div>
+    </section>
+  )
+}
+
+export default function Cart({shipPrice}){
+  const [cartItems, setCartItems] = useState(initialCartItems)
+  // Plus
+  function handlePlusQuantity(itemId){
+    setCartItems(cartItems.map(cartItem => {
+      if(cartItem.id === itemId){
+        return {
+          ...cartItem,
+          quantity: cartItem.quantity + 1
+        };
+      } else {
+        return cartItem;
+      }
+    }))
   }
-]
 
-function CartItem(){
-  const cartItemList = cartItem.map(data =>
-    <div className="product-container col col-12" data-count="0" data-price={data.price} key={data.name}>
-      <img className="img-container" src={data.img} alt={data.name}/>
-      <div className="product-info">
-        <div className="product-info-base">
-          <div className="product-name">{data.name}</div>
-          <div className="product-control-container">
-              <div className="product-control">
-                <IconMinus className="product-action minus"/>
-                <span className="product-count">{data.quantity}</span>
-                <IconPlus className="product-action plus"/>
+  // Minus
+  function handleMinusQuantity(itemId){
+    let nextCartItems = cartItems.map(cartItem => {
+      if(cartItem.id === itemId){
+        return{
+          ...cartItem,
+          quantity: cartItem.quantity - 1
+        };
+      } else {
+        return cartItem;
+      }
+    });
+    nextCartItems = nextCartItems.filter(item => item.quantity > 0);
+    setCartItems(nextCartItems)
+  }
+  const totalPrice = cartItems.reduce((sum, { price, quantity }) => sum + price * quantity, 0)
+
+  // return value
+  return(
+    <>
+      <section className="product-list col col-12" data-total-price={totalPrice}>
+        {
+          cartItems.map(cartItem => (
+            <div className="product-container col col-12" data-count={cartItem.price*cartItem.quantity} data-price={cartItem.price} key={cartItem.name}>
+              <img className="img-container" src={cartItem.img} alt={cartItem.name}/>
+              <div className="product-info">
+                <div className="product-info-base">
+                  <div className="product-name">{cartItem.name}</div>
+                  <div className="product-control-container">
+                      <div className="product-control">
+                        <IconMinus className="product-action minus cursor-point" onClick={
+                          () => {handleMinusQuantity(cartItem.id)}
+                        }/>
+                        <span className="product-count">{cartItem.quantity}</span>
+                        <IconPlus className="product-action plus cursor-point" onClick={
+                          () => {handlePlusQuantity(cartItem.id)}
+                        }/>
+                      </div>
+                  </div>
+                </div>
+                <div className="price">${' '}{cartItem.price*cartItem.quantity}</div>
               </div>
-          </div>
-        </div>
-        <div className="price">{data.price}</div>
-      </div>
-    </div>
-  )
-  return(
-    <section className="product-list col col-12" data-total-price="0">
-      {cartItemList}
-    </section>
-  )
-}
-function CartInfo(){
-  const cartInfoList = cartInfo.map(data =>
-    <section className={data.type} key={data.text}>
-      <div className="text">{data.text}</div>
-      <div className="price">{data.price}</div>
-    </section>
-  )
-  return(
-    <>
-      {cartInfoList}
+            </div>
+          ))
+        }
+      </section>
+      <CartInfo type="shipping" text='運費' price="免費"/>
+      <CartInfo type="total" text='小計' price={totalPrice}/>
     </>
   )
 }
 
 
-export default function Cart(){
-  return(
-    <>
-      <CartItem/>
-      <CartInfo/>
-    </>
-  )
-}
+
+
+
